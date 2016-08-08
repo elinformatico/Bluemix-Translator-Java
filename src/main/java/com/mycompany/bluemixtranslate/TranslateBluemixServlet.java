2c9194176e27d4bc9c1cf93dea915c84184e2c71
elinformatico <donoe1985@gmail.com>
2016-08-08 17:05:11 -0500

    Proyecto inicial Funcionando

---
+++ src/main/java/com/mycompany/bluemixtranslate/TranslateBluemixServlet.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.bluemixtranslate;
+
+import com.ibm.watson.developer_cloud.language_translation.v2.LanguageTranslation;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Noe
+ */
+@WebServlet(name = "TranslateBluemixServlet", urlPatterns = {"/TranslateBluemixServlet"})
+public class TranslateBluemixServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        try (PrintWriter out = response.getWriter()) {
+           
+            LanguageTranslation service = new LanguageTranslation();
+            String user = "adb4e6aa-126b-4c78-9990-c4dd9c95c93e";
+            String pass = "gdvTbYUlklGq";
+            service.setUsernameAndPassword(user, pass);        
+            String fraseATraducir = request.getParameter("frase");
+            String traduccion = service.translate(fraseATraducir,"en-es").getFirstTranslation();
+            out.println("<h1>Traducción:" + traduccion + "</h1>");                
+
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
